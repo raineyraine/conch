64abb7c03a6b2c5cbdd35d15a02e4bfa1d2107de
Raine <randomrandom.epic.epic@gmail.com>
2026-02-14 14:21:29 -0500

    prep for publish

--- pkgs/conch_ui/src/index.d.ts
+++ pkgs/conch_ui/src/index.d.ts
@@ -1,31 +1,26 @@
 import type Vide from "@rbxts/vide";
 
-export = ConchUi;
-export as namespace ConchUi;
+/**
+ * Binds the activation of Conch to a given user input or keycode. This automatically mounts the UI if it has not yet been mounted.
+ */
+export function bind_to(input: Enum.KeyCode | Enum.UserInputType): void;
 
-declare namespace ConchUi {
-	/**
-	 * Binds the activation of Conch to a given user input or keycode. This automatically mounts the UI if it has not yet been mounted.
-	 */
-	function bind_to(input: Enum.KeyCode | Enum.UserInputType): void;
+/**
+ * Mounts the UI to the PlayerGui.
+ */
+export function mount(): void;
 
-	/**
-	 * Mounts the UI to the PlayerGui.
-	 */
-	function mount(): void;
+/**
+ * A vide source that stores if the UI is enabled or not. When called with a boolean, sets it to that boolean. Otherwise, returns the currently stored boolean.
+ */
+export const opened: Vide.Source<boolean>;
 
-	/**
-	 * A vide source that stores if the UI is enabled or not. When called with a boolean, sets it to that boolean. Otherwise, returns the currently stored boolean.
-	 */
-	const opened: Vide.Source<boolean>;
+/**
+ * Refers to the UI component for the Conch UI.
+ */
+export function app(): ScreenGui;
 
-	/**
-	 * Refers to the UI component for the Conch UI.
-	 */
-	function app(): ScreenGui;
-
-	/**
-	 * Reference to Conch package
-	 */
-	const conch: void;
-}
+/**
+ * Reference to Conch package
+ */
+export const conch: void;
